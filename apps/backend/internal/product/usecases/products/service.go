426cbfc1cc28ea1ba03ed213cf417bd99fc785f3
Стив <kenethra.74@gmail.com>
2023-03-29 06:08:06 -0600

    fix(backend): service use cases

--- apps/backend/internal/product/usecases/products/service.go
+++ apps/backend/internal/product/usecases/products/service.go
@@ -3,25 +3,42 @@ package products
 import (
 	"context"
 	"github/karchx/coffee-system/internal/product/domain"
+	"strings"
 
+	"github.com/google/wire"
 	"github.com/pkg/errors"
 )
 
-
 var _ UseCase = (*service)(nil)
 
+var UseCaseSet = wire.NewSet(NewService)
 
 type service struct {
-  repo domain.ProductRepo
+	repo domain.ProductRepo
+}
+
+func NewService(repo domain.ProductRepo) UseCase {
+	return &service{
+		repo: repo,
+	}
 }
 
 func (s *service) GetItemTypes(ctx context.Context) ([]*domain.ItemTypeDto, error) {
-  results, err := s.repo.GetAll(ctx)
-  if err != nil {
-    return nil, errors.Wrap(err, "service.GetItemTypes")
-  }
+	results, err := s.repo.GetAll(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "service.GetItemTypes")
+	}
 
-  return results,nil
+	return results, nil
 }
 
-func (s *service) GetItemsBYTypes() {}
+func (s *service) GetItemsByType(ctx context.Context, itemTypes string) ([]*domain.ItemDto, error) {
+	types := strings.Split(itemTypes, ",")
+
+	results, err := s.repo.GetByTypes(ctx, types)
+	if err != nil {
+		return nil, errors.Wrap(err, "service.GetItemsByType")
+	}
+
+	return results, nil
+}
